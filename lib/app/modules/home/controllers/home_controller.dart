9ab8f4d1853b9d15794017955e8c482884077e00
PratikFZ <pratik.fz@outlook.com>
2025-05-20 22:21:11 +0530

    modified:   lib/app/modules/home/controllers/home_controller.dart

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,131 +1,68 @@
-import 'dart:async';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
-
 import 'package:get/get.dart';
-// import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../../../data/models/article_model.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/services.dart' show rootBundle;
-// import 'package:shared_preferences/shared_preferences.dart';
 
-class HomeController extends GetxController {
+class NewsSearchController extends GetxController {
   final articles = <Article>[].obs;
   final isLoading = true.obs;
-  final selectedCategory = 'general'.obs;
-  var isInternetConnected = false.obs;
-  List<ConnectivityResult> connectionStatus = [ConnectivityResult.none].obs;
-  final Connectivity _connectivity = Connectivity();
-  late StreamSubscription<List<ConnectivityResult>> _connectivitySubscription;
-
-  final categories = [
-    'general',
-    'business',
-    'technology',
-    'sports',
-    'entertainment',
-    'health',
-    'science'
-  ];
+  final selectedCategory = 'All'.obs;
+  final searchQuery = ''.obs;
 
   @override
   void onInit() {
     super.onInit();
-    loadSavedNews();
     fetchNews();
-    initConnetivity();
-    _connectivitySubscription =
-        _connectivity.onConnectivityChanged.listen(_updateConnectivity);
-  }
-
-  Future<void> loadSavedNews() async {
-    try {
-      final directory = await getApplicationDocumentsDirectory();
-      final filePath = '${directory.path}/fallback_news.json';
-      final file = File(filePath);
-
-      if (await file.exists()) {
-        // Load news from the updated JSON file
-        final savedNews = await file.readAsString();
-        final jsonData = json.decode(savedNews);
-        articles.value = (jsonData['articles'] as List)
-            .map((article) => Article.fromJson(article))
-            .toList();
-      } else {
-        // Fallback to the original JSON file in assets
-        final fallbackData =
-            await rootBundle.loadString('assets/fallback_news.json');
-        final jsonData = json.decode(fallbackData);
-        articles.value = (jsonData['articles'] as List)
-            .map((article) => Article.fromJson(article))
-            .toList();
-      }
-    } catch (e) {
-      Get.snackbar('Error', 'Failed to load saved news: $e');
-    }
-  }
-
-  Future<void> initConnetivity() async {
-    late List<ConnectivityResult> result;
-
-    try {
-      result = await _connectivity.checkConnectivity();
-    } catch (e) {
-      // ignore: avoid_print
-      print("Error : $e");
-    }
-
-    if (isClosed) {
-      // ignore: null_argument_to_non_null_type
-      return Future.value(null);
-    }
-    return _updateConnectivity(result);
-  }
-
-  Future<void> _updateConnectivity(List<ConnectivityResult> result) async {
-    connectionStatus = result;
-    isInternetConnected.value = result.contains(ConnectivityResult.wifi) ||
-        result.contains(ConnectivityResult.mobile);
-    fetchNews();
-  }
-
-  @override
-  void onClose() {
-    _connectivitySubscription.cancel();
-    super.onClose();
   }
 
   Future<void> fetchNews() async {
     isLoading.value = true;
+    const apiKey = '6fc5e32fab30444392f12ce281eb98b4';
+    String url;
+
+    if (searchQuery.value.isEmpty) {
+      url = 'https://newsapi.org/v2/top-headlines?country=us';
+      if (selectedCategory.value.toLowerCase() != 'all') {
+        url += '&category=${selectedCategory.value.toLowerCase()}';
+      }
+    } else {
+      url = 'https://newsapi.org/v2/everything?q=${searchQuery.value}';
+    }
 
     try {
-      // Load news from the fallback JSON file
-      final fallbackData =
-          await rootBundle.loadString('assets/fallback_news.json');
-      final jsonData = json.decode(fallbackData);
-      articles.value = (jsonData['articles'] as List)
-          .map((article) => Article.fromJson(article))
-          .toList();
-
-      // Update the JSON file with the fetched news
-      final directory = await getApplicationDocumentsDirectory();
-      final filePath = '${directory.path}/fallback_news.json';
-      final file = File(filePath);
-
-      // Write the updated news data to the file
-      await file.writeAsString(json.encode({'articles': articles}));
-
-      Get.snackbar('Notice', 'News data updated in JSON file.');
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'x-api-key': apiKey, // Add the API key as a header
+        },
+      );
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        articles.value = (jsonData['articles'] as List)
+            .map((article) => Article.fromJson(article))
+            .where((article) =>
+                article.title != '[Removed]' &&
+                (article.content != '[Removed]' || article.content != '') &&
+                article.description != '[Removed]')
+            .toList();
+      } else {
+        throw Exception('Failed to load news');
+      }
     } catch (e) {
-      Get.snackbar('Error', 'Failed to load or update news: $e');
+      Get.snackbar('Error', 'Failed to fetch news: $e');
     } finally {
       isLoading.value = false;
     }
   }
 
+  void search(String query) {
+    searchQuery.value = query;
+    fetchNews();
+  }
+
   void changeCategory(String category) {
     selectedCategory.value = category;
+    searchQuery.value = '';
     fetchNews();
   }
 }
